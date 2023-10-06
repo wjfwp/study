fd46915b6c4d8f756d03b380a1f8fe02e4610054
wjfwp <sis6710@gmail.com>
2023-10-06 20:51:07 +0900

    23/10/06

--- JavaWork/spring/src/spring/di/Program.java
+++ JavaWork/spring/src/spring/di/Program.java
@@ -1,10 +1,9 @@
 package spring.di;
 
-import spring.di.entity.Exam;
-import spring.di.entity.NewlecExam;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import spring.di.ui.ExamConsole;
-import spring.di.ui.GridExamConsole;
-import spring.di.ui.InlineExamConsole;
 
 public class Program {
 
@@ -17,8 +16,12 @@ public class Program {
 		console.setExam(exam);
 		*/
 		
-		ExamConsole console = ?;
-		console.print();
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		
+		//ExamConsole console = (ExamConsole)context.getBean("console");
+		ExamConsole console = context.getBean(ExamConsole.class);
+		
+		console.print();	
 	}
 
 }
